6916ade5ec335fbb3e215a6ccb1bff37535b0dbe
naksh781 <160005047+naksh781@users.noreply.github.com>
2024-02-16 08:50:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ function log() {
     setResult(Math.log10(getResult()));
 }
 
+function sin() {
+    setResult(Math.sin(getResult()));
+}
+
+function cos() {
+    setResult(Math.cos(getResult()));
+}
+
+function tan() {
+    setResult(Math.tan(getResult()));
+}
+
 function ln() {
     setResult(Math.log(getResult()));
 }
@@ -64,7 +76,6 @@ function abs() {
     setResult(Math.abs(getResult()));
 }
 
-// Conversion functions
 function toDegrees(radians) {
     return radians * (180 / Math.PI);
 }
@@ -73,27 +84,16 @@ function toRadians(degrees) {
     return degrees * (Math.PI / 180);
 }
 
-// Trigonometric functions
-function sin() {
-    setResult(Math.sin(toRadians(getResult())));
-}
-
-function cos() {
-    setResult(Math.cos(toRadians(getResult())));
-}
-
-function tan() {
-    setResult(Math.tan(toRadians(getResult())));
-}
-
 function arcsin() {
-    setResult(toDegrees(Math.asin(getResult())));
+    setResult(toDegrees(Math.asin(toRadians(getResult()))));
 }
 
 function arccos() {
-    setResult(toDegrees(Math.acos(getResult())));
+    setResult(toDegrees(Math.acos(toRadians(getResult()))));
 }
 
 function arctan() {
-    setResult(toDegrees(Math.atan(getResult())));
+    setResult(toDegrees(Math.atan(toRadians(getResult()))));
 }
+
+
