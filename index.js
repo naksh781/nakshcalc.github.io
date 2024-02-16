cd87634c39def18ebcac4ee1d5fc9c7fb97d6eb8
naksh781 <160005047+naksh781@users.noreply.github.com>
2024-02-16 08:29:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,18 +32,6 @@ function log() {
     setResult(Math.log10(getResult()));
 }
 
-function sin() {
-    setResult(Math.sin(getResult()));
-}
-
-function cos() {
-    setResult(Math.cos(getResult()));
-}
-
-function tan() {
-    setResult(Math.tan(getResult()));
-}
-
 
 function ln() {
     setResult(Math.log(getResult()));
@@ -85,15 +73,31 @@ function toRadians(degrees) {
     return degrees * (Math.PI / 180);
 }
 
-function arcsin() {
-    setResult(toDegrees(Math.asin(toRadians(getResult()))));
+
+function sin() {
+    setResult(Math.sin(toRadians(getResult())));
 }
+
+function cos() {
+    setResult(Math.cos(toRadians(getResult())));
+}
+
+
+function tan() {
+    setResult(Math.tan(toRadians(getResult())));
+}
+
+
+function arcsin() {
+    setResult(toDegrees(Math.asin(getResult())));
+}
+
 
 function arccos() {
-    setResult(toDegrees(Math.acos(toRadians(getResult()))));
+    setResult(toDegrees(Math.acos(getResult())));
 }
+
 
 function arctan() {
-    setResult(toDegrees(Math.atan(toRadians(getResult()))));
+    setResult(toDegrees(Math.atan(getResult())));
 }
-
